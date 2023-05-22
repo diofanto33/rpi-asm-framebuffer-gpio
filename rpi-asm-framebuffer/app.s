ab356956061fdafe6c147b4d431d3f9d420767b6
Diego Rodríguez <91631854+diofanto33@users.noreply.github.com>
2023-05-22 01:02:47 -0300

    [UPD] squared_bg call

--- rpi-asm-framebuffer/app.s
+++ rpi-asm-framebuffer/app.s
@@ -6,8 +6,30 @@
 
 main:
     mov x20, x0
-
+    
     bl squared_bg
-		
+    
+    // Ejemplo de uso de gpios
+    mov x9, GPIO_BASE
+
+    // Atención: se utilizan registros w porque la documentación de broadcom
+    // indica que los registros que estamos leyendo y escribiendo son de 32 bits
+
+    // Setea gpios 0 - 9 como lectura
+    str wzr, [x9, GPIO_GPFSEL0]
+
+    // Lee el estado de los GPIO 0 - 31
+    ldr w10, [x9, GPIO_GPLEV0]
+
+    // And bit a bit mantiene el resultado del bit 2 en w10 (notar 0b... es binario)
+    // al inmediato se lo refiere como "máscara" en este caso:
+    // - Al hacer AND revela el estado del bit 2
+    // - Al hacer OR "setea" el bit 2 en 1
+    // - Al hacer AND con el complemento "limpia" el bit 2 (setea el bit 2 en 0)
+    and w11, w10, 0b00000010
+    // si w11 es 0 entonces el GPIO 1 estaba liberado
+    // de lo contrario será distinto de 0, (en este caso particular 2)
+    // significando que el GPIO 1 fue presionado
+	
 InfLoop:
     b InfLoop
