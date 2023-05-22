3f4e4caed5bd184cde088bee21199d485a0febd1
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-22 00:49:37 -0300

    [ADD] Proc squared_bg

--- rpi-asm-framebuffer/utils.s
+++ rpi-asm-framebuffer/utils.s
@@ -1,6 +1,8 @@
 .ifndef utils_s
 .equ utils_s, 0
 
+.include "data.s"
+
 /*
 	@brief 
 	The proc map put in x0 the address of the pixel in coordinates (x3, x4)
@@ -63,6 +65,53 @@ draw_square_loop2:
 	ldr x30, [sp]
 	add sp, sp, #40
 	br x30
-	
+
+/*
+	@brief:
+	draw a squared background in the frame buffer with the color in x10 and x11
+	@param: None
+	@save_stack: x1, x2, x8, x10, x11, x30
+*/
+
+
+squared_bg:
+    sub sp,sp,#48
+	str x11, [sp, #40]
+	str x10, [sp, #32]
+	str x8, [sp, #24]
+	str x2, [sp, #16]
+	str x1, [sp, #8] 
+	str x30, [sp]
+
+	mov x0, x20;
+    
+	ldr x10, bluecolorbg1
+	ldr x11, bluecolorbg2
+								// x0 direciona un color
+    add x8, x0, #4            	// x8 direcciona el otro color
+    mov x2, SCREEN_HEIGH     	// y size
+square_bg_loop1:
+    mov x1, SCREEN_WIDTH     	// x size
+square_bg_loop0:
+	stur w10, [x0]           	// set color of pixel n
+    stur w11, [x8]           	// set color of pixel n
+    add x8, x8, #8            	// next + 1 pixel
+    add x0, x0, #8            	// next + 1 pixel
+    sub x1, x1, #2	         	// decrement x counter
+    cbnz x1, square_bg_loop0	// if not end row jump
+    eor x0, x0, x8           	// swap(x0,x8)
+    eor x8, x0, x8
+    eor x0, x0, x8
+    sub x2, x2, #1            	// decrement y counter
+    cbnz x2, square_bg_loop1    // if not last row, jump
+
+	ldr x11, [sp, #40]
+	ldr x10, [sp, #32]
+	ldr x8, [sp, #24]
+	ldr x2, [sp, #16]
+	ldr x1, [sp, #8]
+    ldr x30, [sp]
+    add sp, sp, #48
+    br x30
 
 .endif
