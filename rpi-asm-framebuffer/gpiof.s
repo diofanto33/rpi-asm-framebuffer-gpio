bf426ae815b92a6ef0562e7cb2be477a85006e7c
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-06-07 14:45:29 -0300

    [ADD] gpio_func

--- rpi-asm-framebuffer/gpiof.s
+++ rpi-asm-framebuffer/gpiof.s
@@ -7,48 +7,48 @@ gpio_func:
     sub sp, sp, #8										
     stur x9, [sp]
 
-	// Seteamos el GPIO para poder realizar la lectura de los inputs
-	mov x9, GPIO_BASE
+    // Seteamos el GPIO para poder realizar la lectura de los inputs
+    mov x9, GPIO_BASE
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+    // Atención: se utilizan registros w porque la documentación de broadcom
+    // indica que los registros que estamos leyendo y escribiendo son de 32 bits
 
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
+    // Setea gpios 0 - 9 como lectura
+    str wzr, [x9, GPIO_GPFSEL0]
 
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
+    // Lee el estado de los GPIO 0 - 31
+    ldr w10, [x9, GPIO_GPLEV0]
 
-	ldr x9, [sp, #8]					 			
-	add sp, sp, #8
+    ldr x9, [sp, #8]					 			
+    add sp, sp, #8
 
-	// And bit a bit mantiene el resultado del bit 2 en w10 (notar 0b... es binario)
-	// al inmediato se lo refiere como "máscara" en este caso:
-	// - Al hacer AND revela el estado del bit 2
-	// - Al hacer OR "setea" el bit 2 en 1
-	// - Al hacer AND con el complemento "limpia" el bit 2 (setea el bit 2 en 0)
+    // And bit a bit mantiene el resultado del bit 2 en w10 (notar 0b... es binario)
+    // al inmediato se lo refiere como "máscara" en este caso:
+    // - Al hacer AND revela el estado del bit 2
+    // - Al hacer OR "setea" el bit 2 en 1
+    // - Al hacer AND con el complemento "limpia" el bit 2 (setea el bit 2 en 0)
 		
-	and w11, w10, 0b00000010
-	and w12, w10, 0b00000100
-	and w13, w10, 0b00001000
-	and w14, w10, 0b00010000
-	and w15, w10, 0b00100000
+    and w11, w10, 0b00000010
+    and w12, w10, 0b00000100
+    and w13, w10, 0b00001000
+    and w14, w10, 0b00010000
+    and w15, w10, 0b00100000
 
-	// si w11 es 0 entonces el GPIO 1 estaba liberado
-	// de lo contrario será distinto de 0, (en este caso particular 2)
-	// significando que el GPIO 1 fue presionado
+    // si w11 es 0 entonces el GPIO 1 estaba liberado
+    // de lo contrario será distinto de 0, (en este caso particular 2)
+    // significando que el GPIO 1 fue presionado
     
-   	cmp w11, key_W			
-	b.eq mov_up
+    cmp w11, key_W			
+    b.eq mov_up
 
-	cmp w12, key_A			
-	b.eq mov_lft
+    cmp w12, key_A			
+    b.eq mov_lft
 
-	cmp w13, key_S			
+    cmp w13, key_S			
     b.eq mov_down
 
-	cmp w14, key_D			
-	b.eq mov_rgt
+    cmp w14, key_D			
+    b.eq mov_rgt
     
     ret 
 
